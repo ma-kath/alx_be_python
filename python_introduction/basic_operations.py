191f5c6025e6a6b48c59df95d095c737051693be
ma-kath <makathosn@gmail.com>
2024-09-14 09:00:09 +0200

    Adding the tasks files

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+summ = number1 + number2
+subs = number1 - number2
+prod = number1 * number2
+print(f"Addition of {number1} and {number2} is {summ}")
+print(f"Substraction of {number1} and {number2} is {subs}")
+print(f"Multiplication of {number1} and {number2} is {prod}")
