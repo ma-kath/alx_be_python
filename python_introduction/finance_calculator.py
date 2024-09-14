a261fe26c00e914bf5da4e89a80c3ef73919d505
ma-kath <makathosn@gmail.com>
2024-09-14 09:27:45 +0200

    Correcting floating type

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses: "))
-m_savings = income - expenses
-p_savings = int(m_savings * 12 + (m_savings * 12 * 0.05))
-print(f"Your monthly savings are ${m_savings}.")
-print(f"Projected savings after one year, with interest, is: ${p_savings}.")
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"Your monthly savings are ${int(monthly_savings)}.")
+print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
