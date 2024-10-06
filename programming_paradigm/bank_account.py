7661de4815a46669e426dd4d4fe848b94500a6dc
ma-kath <makathosn@gmail.com>
2024-10-06 23:17:56 +0200

    prog_paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.balance = account_balance
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        can_withdraw = 0 < amount <= self.balance
+        if can_withdraw:
+            self.balance -= amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f'Current Balance: ${self.balance:.2f}')
